eb67f948c1d882007412ea92f3bee201473bc88d
guanzhengWang <610085827@qq.com>
2017-01-10 11:44:44 +0800

    UpdateProject

--- app/src/main/java/cn/ucai/fulicenter/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/MainActivity.java
@@ -3,6 +3,8 @@ package cn.ucai.fulicenter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.ucai.fulicenter.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
