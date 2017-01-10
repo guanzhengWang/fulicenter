72743a8c63661e885b705f5762d9cdffc039362b
guanzhengWang <610085827@qq.com>
2017-01-10 15:40:47 +0800

    add splash

---
+++ app/src/main/java/cn/ucai/fulicenter/view/MFGT.java
@@ -0,0 +1,22 @@
+package cn.ucai.fulicenter.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import cn.ucai.fulicenter.R;
+
+/**
+ * Created by Administrator on 2017/1/10 0010.
+ */
+
+public class MFGT {
+    public static void finish(Activity activity){
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+    }
+    public static void startActivity(Activity context, Class<?> cla){
+        context.startActivity(new Intent(context,cla));
+        context.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+}
