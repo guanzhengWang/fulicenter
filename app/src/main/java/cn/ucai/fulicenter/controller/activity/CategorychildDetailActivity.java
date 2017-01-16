8d37ae67bc4ec963edbf67c16651717ed638be91
guanzhengWang <610085827@qq.com>
2017-01-16 11:43:28 +0800

    修正箭头

--- app/src/main/java/cn/ucai/fulicenter/controller/activity/CategorychildDetailActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/CategorychildDetailActivity.java
@@ -1,13 +1,14 @@
 package cn.ucai.fulicenter.controller.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,14 +16,11 @@ import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.application.I;
 import cn.ucai.fulicenter.controller.fragment.NewGoodsFragment;
-import cn.ucai.fulicenter.model.net.ModelNewGoods;
 
 public class CategorychildDetailActivity extends AppCompatActivity {
 
     @BindView(R.id.ivBack)
     ImageView ivBack;
-    @BindView(R.id.tvTitle)
-    TextView tvTitle;
     @BindView(R.id.alertTitle)
     RelativeLayout alertTitle;
     @BindView(R.id.fragment_container)
@@ -30,14 +28,19 @@ public class CategorychildDetailActivity extends AppCompatActivity {
     @BindView(R.id.activity_categorychild_detail)
     LinearLayout activityCategorychildDetail;
     NewGoodsFragment mNewGoodsFragment;
-    boolean priceAsc =false;
-    boolean addtimeAsc=false;
+    boolean priceAsc = false;
+    boolean addtimeAsc = false;
+    @BindView(R.id.sort_addtime)
+    Button sortAddtime;
+    @BindView(R.id.sort_price)
+    Button sortPrice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorychild_detail);
         ButterKnife.bind(this);
-        mNewGoodsFragment=new NewGoodsFragment();
+        mNewGoodsFragment = new NewGoodsFragment();
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fragment_container, mNewGoodsFragment)
                 .commit();
@@ -49,23 +52,33 @@ public class CategorychildDetailActivity extends AppCompatActivity {
 
     @OnClick({R.id.sort_addtime, R.id.sort_price})
     public void onClick(View view) {
-        int sortBy=I.SORT_BY_ADDTIME_ASC;
+        int sortBy = I.SORT_BY_ADDTIME_ASC;
+        Drawable up = getResources().getDrawable(R.drawable.arrow_order_up);
+        Drawable down = getResources().getDrawable(R.drawable.arrow_order_down);
         switch (view.getId()) {
             case R.id.sort_addtime:
-                if(addtimeAsc) {
-                    sortBy=I.SORT_BY_ADDTIME_ASC;
-                }else {
-                    sortBy=I.SORT_BY_ADDTIME_DESC;
+                if (addtimeAsc) {
+                    sortBy = I.SORT_BY_ADDTIME_ASC;
+                    up.setBounds(0,0,up.getIntrinsicWidth(),up.getIntrinsicHeight());
+                    sortAddtime.setCompoundDrawables(null,null,up,null);
+                } else {
+                    sortBy = I.SORT_BY_ADDTIME_DESC;
+                    down.setBounds(0,0,up.getIntrinsicWidth(),up.getIntrinsicHeight());
+                    sortAddtime.setCompoundDrawables(null,null,down,null);
                 }
-                addtimeAsc=!addtimeAsc;
+                addtimeAsc = !addtimeAsc;
                 break;
             case R.id.sort_price:
-                if(priceAsc) {
-                    sortBy=I.SORT_BY_PRICE_ASC;
-                }else {
-                    sortBy=I.SORT_BY_PRICE_DESC;
+                if (priceAsc) {
+                    sortBy = I.SORT_BY_PRICE_ASC;
+                    up.setBounds(0,0,up.getIntrinsicWidth(),up.getIntrinsicHeight());
+                    sortPrice.setCompoundDrawables(null,null,up,null);
+                } else {
+                    sortBy = I.SORT_BY_PRICE_DESC;
+                    down.setBounds(0,0,up.getIntrinsicWidth(),up.getIntrinsicHeight());
+                    sortPrice.setCompoundDrawables(null,null,down,null);
                 }
-                priceAsc=!priceAsc;
+                priceAsc = !priceAsc;
                 break;
         }
         mNewGoodsFragment.sortGoods(sortBy);
