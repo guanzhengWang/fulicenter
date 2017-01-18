aef3b5139da8fc2a81f1d45eba6f8c6fda1a9b1c
guanzhengWang <610085827@qq.com>
2017-01-18 19:08:50 +0800

    初步实现上传头像功能(上传失败)

--- app/src/main/java/cn/ucai/fulicenter/controller/activity/SettingActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/SettingActivity.java
@@ -1,12 +1,16 @@
 package cn.ucai.fulicenter.controller.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,10 +18,15 @@ import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.application.FuLiCenterApplication;
 import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.model.bean.Result;
 import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.net.IModelUser;
+import cn.ucai.fulicenter.model.net.ModelUser;
+import cn.ucai.fulicenter.model.net.OnCompleteListener;
 import cn.ucai.fulicenter.model.utils.CommonUtils;
 import cn.ucai.fulicenter.model.utils.ImageLoader;
 import cn.ucai.fulicenter.model.utils.OnSetAvatarListener;
+import cn.ucai.fulicenter.model.utils.ResultUtils;
 import cn.ucai.fulicenter.model.utils.SharePrefrenceUtils;
 import cn.ucai.fulicenter.view.DisplayUtils;
 import cn.ucai.fulicenter.view.MFGT;
@@ -31,6 +40,7 @@ public class SettingActivity extends AppCompatActivity {
     @BindView(R.id.tv_user_nick)
     TextView tvUserNick;
     OnSetAvatarListener mOnSetAvatarListener;
+    IModelUser model;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -91,5 +101,38 @@ public class SettingActivity extends AppCompatActivity {
     }
 
     private void uploadAvatar() {
+        final ProgressDialog dialog=new ProgressDialog(this);
+        dialog.setMessage(getString(R.string.update_user_avatar));
+        dialog.show();
+        User user=FuLiCenterApplication.getUser();
+        model=new ModelUser();
+        File file=null;
+        file=new File(String.valueOf(OnSetAvatarListener.
+                getAvatarFile(this,I.AVATAR_TYPE_USER_PATH+"/"+user.getMuserName()+user.getMavatarSuffix())));
+        model.UploadAvatar(this,
+               user.getMuserName(), file,
+                new OnCompleteListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        Log.e("uploadAvatar","s"+s);
+                        int msg=R.string.update_user_avatar_fail;
+                        if(s!=null){
+                            Result result = ResultUtils.getResultFromJson(s, User.class);
+                            if(result!=null){
+                                if(result.isRetMsg()){
+                                    msg=R.string.update_user_avatar_success;
+                                }
+                            }
+                        }
+                        CommonUtils.showLongToast(msg);
+                        dialog.dismiss();
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        CommonUtils.showLongToast(error);
+                        dialog.dismiss();
+                    }
+                });
     }
 }
